82e020b32f51ff0ec65314be72cfebe59a179cd4
younes ismaili <ismailiyounes20@gmail.com>
2024-03-14 08:41:21 -0400

    update in jenkinsfile

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ docker login --username "$DOCKER_USERNAME" --password "$DOCKER_PASSWORD" $DOCKER
 # Check if Docker registry is already running
 if [ ! "$(docker ps -q -f name=registry)" ]; then
     # Start local Docker registry
-    docker run -d -p 5000:5000 --restart=always --name registry registry:1
+    docker run -d -p 5000:5000 --restart=always --name registry registry:2
 fi
 
 mkdir -p postgres
