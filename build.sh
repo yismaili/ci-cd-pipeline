08cd97d0e55b53eb0340fe6cb1ca8e3ded53a73e
younes ismaili <ismailiyounes20@gmail.com>
2024-04-08 08:47:53 -0400

    update in jenkinsfile

--- build.sh
+++ build.sh
@@ -1,7 +1,16 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-DOCKER_USERNAME="yismaili"
-DOCKER_PASSWORD="pass1227@"
-DOCKER_REGISTRY="https://index.docker.io/v1/"
+# Set Docker credentials as environment variables
+export DOCKER_USERNAME="yismaili"
+export DOCKER_PASSWORD="pass1227@"
+export DOCKER_REGISTRY="https://index.docker.io/v1/"
 
+# Log in to Docker registry
 docker login --username "$DOCKER_USERNAME" --password "$DOCKER_PASSWORD" $DOCKER_REGISTRY
+
+# Check if the Docker registry container already exists
+if docker ps -a --format '{{.Names}}' | grep -q "^registry$"; then
+    echo "Docker registry container 'registry' already exists."
+else
+    docker run -d -p 5000:5000 --restart=always --name registry registry:2
+fi
