dad8918b9c9afc0613dd5825ffd5ba021922682e
younes ismaili <ismailiyounes20@gmail.com>
2024-05-07 10:19:46 -0400

    create provision yaml file

--- deployment.sh
+++ deployment.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 ssh-keygen -t ed25519 -f ~/.ssh/id_ansible -N ''
-ssh-copy-id -i ~/.ssh/id_ansible yoyo@192.168.11.102
+ssh-copy-id -i ~/.ssh/id_ansible yoyo@192.168.100.74
 
 ansible-playbook -i inventory.yml deploy.yaml --ask-become-pass
