dd806314c48f8032c20ce593fb73d21ee59888bc
younes ismaili <ismailiyounes20@gmail.com>
2024-05-13 10:15:17 -0400

    ++

--- deployment.sh
+++ deployment.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 ssh-keygen -t ed25519 -f ~/.ssh/id_ansible -N ''
-ssh-copy-id -i ~/.ssh/id_ansible yoyo@192.168.100.74
+ssh-copy-id -i ~/.ssh/id_ansible yoyo@192.168.100.76
 
 ansible-playbook -i inventory.yml deploy.yaml --ask-become-pass
 
